6a31833bf2a19dbf3e5d03d29406dd496bf69e64
Mark Huot <mark@markhuot.com>
2017-02-09 15:56:29 -0500

    fixing comments

--- webpack.config.vue.js
+++ webpack.config.vue.js
@@ -16,14 +16,8 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this nessessary.
-            // 'scss': 'vue-style-loader!css-loader!sass-loader',
-            // 'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
             css: ExtractTextPlugin.extract({fallback: 'isomorphic-style-loader', use: 'css-loader'})
           }
-          // other vue-loader options go here
         }
       },
       {
